eae7045d896b7050cc00678ce5bdb4f3ec4912dd
Sandeep Jagtap <sajagtap88@gmail.com>
2014-07-29 14:16:39 +0530

    Code update for correct values

--- BlackScholes.java
+++ BlackScholes.java
@@ -6,12 +6,12 @@ public class BlackScholes {
 		// TODO Auto-generated method stub
 
 
-		double	K=7900.0 ;  // Strike Price  
-		double	S=7800.0 ;// Spot Price
-		double	T=0.073972603; // E*piray days  in year   = ( option e*piration date - today's date) /365
-		double	R=0.1 ; // Risk Free Rate ,constant value
-		double	V=0.125310271; // Stock volatility
-		double  dR=0.21; //Dividend rate   ( its needs to be set at the start of the month )
+		double	K=7200.0 ;  // Strike Price  
+		double	S=7500.0 ;// Spot Price
+		double	T=0.123287671; // E*piray days  in year   = ( option e*piration date - today's date) /365
+		double	R=0.08 ; // Risk Free Rate ,constant value
+		double	V=0.172479149045707; // Stock volatility
+		double  dR=0.0; //Dividend rate   ( its needs to be set at the start of the month )
 		
 
 		double  D1 ,D2 ,C ,DeltaC,P,DeltaP;
@@ -44,7 +44,7 @@ public class BlackScholes {
 		else{
 			//Case2: if we have dividend rate 
 			//Log (S/K) + [( R -dR + 0.5(V^2) )T]		
-			D1=Math.log(S/K)+ ( R -dR + 0.5*Math.pow(V,2))*T/(V*Math.sqrt(T));
+			D1=(Math.log(S/K)+ ( R -dR + 0.5*Math.pow(V,2))*T)/(V*Math.sqrt(T));
 			D2=D1-V*Math.sqrt(T);
 			
 			//Case2: if we have dividend rate (dR1)
@@ -52,10 +52,12 @@ public class BlackScholes {
 			C=S*Distribution.NORMSDIST(D1)*Math.exp(-dR*T)-(K*Math.exp(-R*T)*Distribution.NORMSDIST(D2));
 			//Delta Call :=  e^(-dRT)*NORMSDIST (D1)
 			DeltaC=Math.exp(-dR*T)*Distribution.NORMSDIST(D1);
-			
+		
 			//Case2: if we have dividend rate (dR1)
-			//=K e^(-RT)(1-NORMSDIST (-D2)) - [S* (1-NORMSDIST (-D1)) * e^(-dRT)]
-		    P=K*Math.exp(-R*T)*(1-Distribution.NORMSDIST(-D2))-(S* (1-Distribution.NORMSDIST(-D1))*Math.exp(-dR*T));
+			
+			//=EXP(-R*T)*K*NORMSDIST(-D2)-EXP(-dR*T)*S*NORMSDIST(-D1))
+			
+		    P=K*Math.exp(-R*T)*Distribution.NORMSDIST(-D2)-(S*Distribution.NORMSDIST(-D1)*Math.exp(-dR*T));
 
 			//Delta Put :=e^(-dRT)*(NORMSDIST(D1)-1)
 		    DeltaP=Math.exp(-dR*T)*(Distribution.NORMSDIST(D1)-1);
@@ -70,10 +72,10 @@ public class BlackScholes {
 		System.out.println("dR:"+dR);
 		
 		System.out.println("---------------------------------");
-		System.out.println("D1:"+D1);
-		System.out.println("ND1:"+Distribution.NORMSDIST(D1));
 		
+		System.out.println("D1:"+D1);
 		System.out.println("D2:"+D2);
+		System.out.println("ND1:"+Distribution.NORMSDIST(D1));
 		System.out.println("ND2:"+Distribution.NORMSDIST(D2));
 		
 		
